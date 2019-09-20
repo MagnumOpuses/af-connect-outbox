2e18a796b1f63b457f01cc0d26ea4090a1fb5035
taz-jt <tazdik.ershad@arbetsformedlingen.se>
2019-09-20 13:22:40 +0200

    outbox service endpoint done

--- route/index.js
+++ route/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const sessionToken = require('./sessionToken');
+const tokenService = require('../service/tokenService');
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
@@ -12,7 +12,10 @@ router.use((req, res, next) => {
 // define the home page route
 router.get('/', (req, res) => res.send(`AF-connnect-outbox is alive`));
 
-// define the about route
-router.post('/sessionToken', sessionToken);
+// define the register session token route
+router.post('/registerToken', tokenService.registerValue);
+
+// define the get the envelop using the sessiontoken
+router.get('/envelop?:sessionToken', tokenService.getValue)
 
 module.exports = router;

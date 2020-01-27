e3a2a0ce2b3e75f48b6059700fff864186693402
taz-jt <tazdik.ershad@arbetsformedlingen.se>
2020-01-27 14:15:23 +0100

    logging features for outbox

--- app/route/index.js
+++ app/route/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const logformat = require('../lib/config/logformat');
 
 const tokenService = require('../service/tokenService');
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
   console.log('Time: ', Date.now());
+  logformat(req, res);
   next();
 });
 
@@ -14,9 +16,6 @@ router.get('/', (req, res) => res.send(`AF-connect-outbox is alive`));
 
 // define the register session token route
 router.post('/store', tokenService.storeValue);
-
-// define the storing value with session token route
-
 
 // define the get the envelop using the sessiontoken
 router.get('/envelop?:sessionToken', tokenService.getValue)
